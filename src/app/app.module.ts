4f61361a9344b55b69238d2fb386e2e6e99da630
Thiaree <thitaree.bam1@gmail.com>
2023-05-18 09:43:46 +0700

    enter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,73 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { RouterModule } from '@angular/router';
+// import {HttpClientModule} from '@angular/common/http';
+// import { ReactiveFormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
+// //import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { AboutComponent } from './components/about/about.component';
+// import { IndexapiComponent } from './components/indexapi/indexapi.component';
+// import { ContactComponent } from './components/contact/contact.component';
+// import { HeaderComponent } from './components/header/header.component';
+// import { FooterComponent } from './components/footer/footer.component';
+// import { PostRoutingModule } from './post/post-routing.module';
+
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     AboutComponent,
+//     IndexapiComponent,
+//     ContactComponent,
+//     HeaderComponent,
+//     FooterComponent,
+    
+    
+//   ],
+//   imports: [
+//     BrowserModule,
+//     FormsModule,
+//     ReactiveFormsModule,
+//     HttpClientModule,
+//     RouterModule.forRoot([
+//       {
+//         path: "",
+//         component: PostRoutingModule
+//       }
+//       // {
+//       //   path: "contact",
+//       //   component: ContactComponent
+//       // },
+//       // {
+//       //   path: "index",
+//       //   component: IndexapiComponent
+//       // }
+//     ])
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import {HttpClientModule} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+   
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './components/about/about.component';
-import { IndexapiComponent } from './components/indexapi/indexapi.component';
-import { ContactComponent } from './components/contact/contact.component';
-
+   
+import { PostModule } from './post/post.module';
+   
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent,
-    IndexapiComponent,
-    ContactComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      {
-        path: "",
-        component: AboutComponent
-      },
-      {
-        path: "contact",
-        component: ContactComponent
-      },
-      {
-        path: "index",
-        component: IndexapiComponent
-      }
-    ])
+    AppRoutingModule,
+    PostModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/indexapi/indexapi.component.ts
+++ src/app/components/indexapi/indexapi.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { DecimalPipe, NgFor } from '@angular/common';
 import {JSONPlaceholderService} from '../../services/jsonplaceholder.service'
-import { NgbPaginationModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+//import { NgbPaginationModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-indexapi',
   templateUrl: './indexapi.component.html',
-  imports: [DecimalPipe,NgFor,FormsModule,NgbPaginationModule,NgbTypeaheadModule],
+  //imports: [DecimalPipe,NgFor,FormsModule,NgbPaginationModule,NgbTypeaheadModule],
   styleUrls: ['./indexapi.component.css']
 })
 export class IndexapiComponent implements OnInit {
@@ -15,12 +15,12 @@ export class IndexapiComponent implements OnInit {
   data: Array<any>
   page = 1;
   pageSize = 10;
-  collectionSize = data.length;
+  //collectionSize = data.length;
 
 
   constructor(private JSONPlaceholder: JSONPlaceholderService){
     this.data = new Array<any>()
-    this.refreshdata();
+    //this.refreshdata();
   }
 
   getDataFromAPI(){
@@ -30,12 +30,12 @@ export class IndexapiComponent implements OnInit {
     })
   }
 
-  refreshdata(){
-    this.data = data.map((data,i) => ({id: i, ...data})).slice(
-      (this.page - 1) * this.pageSize,
-      (this.page - 1) * this.pageSize + this.pageSize
-    )
-  }
+  // refreshdata(){
+  //   this.data = data.map((data,i) => ({id: i, ...data})).slice(
+  //     (this.page - 1) * this.pageSize,
+  //     (this.page - 1) * this.pageSize + this.pageSize
+  //   )
+  // }
 
   ngOnInit(): void {
   }
